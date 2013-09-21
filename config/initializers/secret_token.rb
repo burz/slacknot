a74d6e16e9f1e8672bce06cf986895bfcca30f26
Anthony Burzillo <burz@jhu.edu>
2013-09-20 20:48:11 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Slacknot::Application.config.secret_key_base = '7b7da93564fe0d4a81bc3f1918af7e4e97da562ed5d424dbd6611355db6baf9780cd06a9da304b246abbb1b2e1394de976537a2fa31a0d881525f5e35f9044a1'
