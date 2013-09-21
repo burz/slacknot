475af0bc29f32a27bb66377734b96dc853b558f7
Anthony Burzillo <burz@jhu.edu>
2013-09-20 21:29:12 -0400

    Add Time_interval model

--- app/models/problem.rb
+++ app/models/problem.rb
@@ -1,3 +1,4 @@
 class Problem < ActiveRecord::Base
   belongs_to :assignment
+  has_many :time_intervals
 end
---
+++ db/migrate/20130921012809_create_time_intervals.rb
@@ -0,0 +1,10 @@
+class CreateTimeIntervals < ActiveRecord::Migration
+  def change
+    create_table :time_intervals do |t|
+      t.datetime :start
+      t.datetime :end
+
+      t.timestamps
+    end
+  end
+end
